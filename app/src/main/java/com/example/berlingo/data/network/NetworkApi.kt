91fde472fca4b039c29c78e03fa9434749d24040
tiam <tiam.abderezai@beenera.de>
2023-11-26 00:47:15 +0100

    #1 Setup Retrofit and request data from BVG

--- app/src/main/java/com/example/berlingo/data/network/NetworkApi.kt
+++ app/src/main/java/com/example/berlingo/data/network/NetworkApi.kt
@@ -11,4 +11,36 @@ interface NetworkApi {
     suspend fun getLocations(
         @Query("") searchQuery: String,
     ): Response<List<LocationResponse>>
+    @GET("stops/900360136?linesOfStops=true&language=en")
+    suspend fun getStops(
+        @Query("") searchQuery: String,
+    ): Response<List<LocationResponse>>
+    @GET("trips/")
+    suspend fun getTrips(
+        @Query("") searchQuery: String,
+    ): Response<List<LocationResponse>>
+//    @GET("locations?poi=false&addresses=false&query=mehringdamm/") TODO
+//    suspend fun getJourneys(
+//        @Query("") searchQuery: String,
+//    ): Response<List<LocationResponse>>
+//    @GET("locations?poi=false&addresses=false&query=mehringdamm/") TODO
+//    suspend fun getLocationsNearby(
+//        @Query("") searchQuery: String,
+//    ): Response<List<LocationResponse>>
+//    @GET("locations?poi=false&addresses=false&query=mehringdamm/") TODO
+//    suspend fun getRadar(
+//        @Query("") searchQuery: String,
+//    ): Response<List<LocationResponse>>
+//    @GET("locations?poi=false&addresses=false&query=mehringdamm/") TODO
+//    suspend fun getReachableFrom(
+//        @Query("") searchQuery: String,
+//    ): Response<List<LocationResponse>>
+    @GET("stops/900360136/departures?duration=10&results=4&linesOfStops=true&remarks=true&language=en")
+    suspend fun getDepartures(
+        @Query("") searchQuery: String,
+    ): Response<List<LocationResponse>>
+    @GET("stops/900360136/arrivals?duration=10&linesOfStops=false&remarks=true&language=en")
+    suspend fun getArrivals(
+        @Query("") searchQuery: String,
+    ): Response<List<LocationResponse>>
 }
